2aa1768107d7bbfc5ef28a5101bfbf865b854048
fs-projects <fs.projectsuk@gmail.com>
2024-02-02 17:42:56 +0530

    Add: tailwind and movie list page route with tmdb integration

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+// import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -8,9 +9,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { ReactNode } from "react";
+import stylesheet from "~/tailwind.css";
+// export const links: LinksFunction = () => [
+//   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+// ];
 
+// We are telling remix that we will use tailwind stylesheet and that it should bundle it for us
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: stylesheet },
 ];
 
 export default function App() {
@@ -23,11 +30,26 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <Layout>
+          <Outlet />
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </Layout>
       </body>
     </html>
+  );
+}
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <nav className="px-10 pt-5">
+        <Link to="/" prefetch="intent" className="text-2xl font-semibold">
+          Movie<span className="text-teal-500">DB</span>
+        </Link>
+      </nav>
+      <main>{children}</main>
+    </>
   );
 }
